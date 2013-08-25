d223d23f765b50653a00c8e7c91ae0cd080a1269
Alex McHale <alex@anticlever.com>
2013-08-25 08:41:02 -0500

    Support redistogo.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,9 @@
-$redis_client = Redis.new
+$redis_client =
+  if ENV["REDISTOGO_URL"].present?
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  else
+    Redis.new(:host => "db")
+  end
+
 $redis = Redis::Namespace.new("com.anticlever.budgets", redis: $redis_client)
